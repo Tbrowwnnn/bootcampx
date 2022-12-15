4bfc9189e7ee422fb535484b5c16c88baf35b82a
Tyler Brown <tyler.brown@telus.com>
2022-12-14 18:58:11 -0700

    update with sanitized code

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,6 +1,18 @@
 const { process_params } = require('express/lib/router');
 const { Pool } = require('pg');
 
+const teacherName = [`%${process.argv[2]}%`];
+
+const queryString = `
+SELECT teachers.name as teachers, cohorts.name as cohorts
+FROM assistance_requests
+JOIN teachers ON teachers.id = assistance_requests.teacher_id
+JOIN students ON students.id = assistance_requests.student_id
+JOIN cohorts ON cohorts.id = students.cohort_id
+WHERE cohorts.name LIKE $1
+GROUP BY teachers.name, cohorts.name;
+`;
+
 const pool = new Pool({
   user: 'labber',
   password: '123',
@@ -9,18 +21,10 @@ const pool = new Pool({
 });
 
 
-pool.query(`
-SELECT teachers.name as teachers, cohorts.name as cohorts
-FROM assistance_requests
-JOIN teachers ON teachers.id = assistance_requests.teacher_id
-JOIN students ON students.id = assistance_requests.student_id
-JOIN cohorts ON cohorts.id = students.cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-GROUP BY teachers.name, cohorts.name;
-`)
+pool.query(queryString, teacherName)
 .then(res => {
-  res.rows.forEach(row => {
-    console.log(`${row.cohorts}: ${row.teachers}`);
+  res.rows.forEach(user => {
+    console.log(`${user.cohorts}: ${user.teachers}`);
   })
 })
 
